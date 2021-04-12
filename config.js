a33c22d1d8ae8175220c17b30503044d82f0963a
Cyopn <77410038+Cyopn@users.noreply.github.com>
2021-04-12 15:13:46 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "prefix": "+",
-    "token": "Nzk4MjMzNDExMjQ4MzI0NjA4.X_yCyw.vu2v9Oc2uQxgumYNmNQ6lzzPdHA",
-    "tokenzeew": "605e80a612abe48b9a28d72e",
-    "inviteURI": "https://discord.com/oauth2/authorize?client_id=798233411248324608&scope=bot&permissions=2113928959",
-    "supportServer": "https://discord.gg/RBFX3KHKDp"
-}
